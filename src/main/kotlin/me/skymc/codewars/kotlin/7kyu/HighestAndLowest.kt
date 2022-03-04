cc81a3ce55b36248dd55e46487569faf306472b4
kirraObj <c@skymc.me>
2022-03-05 00:19:54 +0800

    update.

---
+++ src/main/kotlin/me/skymc/codewars/kotlin/7kyu/HighestAndLowest.kt
@@ -0,0 +1,11 @@
+@file:Suppress("PackageName")
+
+package me.skymc.codewars.kotlin.`7kyu`
+
+object HighestAndLowest {
+
+    fun highAndLow(numbers: String): String {
+        val sorted =  numbers.split(" ").map { it.toInt() }.sorted()
+        return "${sorted.last()} ${sorted.first()}"
+    }
+}
