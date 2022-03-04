358060fc4b840d8070637d08971921df9d4c2d2e
kirraObj <c@skymc.me>
2022-03-05 00:40:09 +0800

    update.

--- src/main/kotlin/me/skymc/codewars/kotlin/7kyu/HighestAndLowest.kt
+++ src/main/kotlin/me/skymc/codewars/kotlin/7kyu/HighestAndLowest.kt
@@ -5,7 +5,7 @@ package me.skymc.codewars.kotlin.`7kyu`
 object HighestAndLowest {
 
     fun highAndLow(numbers: String): String {
-        val sorted =  numbers.split(" ").map { it.toInt() }.sorted()
+        val sorted = numbers.split(" ").map { it.toInt() }.sorted()
         return "${sorted.last()} ${sorted.first()}"
     }
 }
---
+++ src/main/kotlin/me/skymc/codewars/kotlin/8kyu/KeepHydrated.kt
@@ -0,0 +1,10 @@
+@file:Suppress("PackageName")
+
+package me.skymc.codewars.kotlin.`8kyu`
+
+object KeepHydrated {
+
+    fun litres(time: Double): Int {
+        return time.toInt() / 2
+    }
+}
