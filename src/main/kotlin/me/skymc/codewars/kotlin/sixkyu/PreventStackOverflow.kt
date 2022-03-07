eadd03a78343758f6e0e055f62b4b3ae2a140c81
kirraObj <c@skymc.me>
2022-03-08 01:09:28 +0800

    update.

---
+++ src/main/kotlin/me/skymc/codewars/kotlin/sixkyu/PreventStackOverflow.kt
@@ -0,0 +1,21 @@
+package me.skymc.codewars.kotlin.sixkyu
+
+import kotlin.random.Random
+
+object PreventStackOverflow {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        repeat(99999) {
+            loop(Random, 99999)
+        }
+    }
+
+    tailrec fun loop(random: Random, int: Int): Int {
+        return if (int <= 0) random.nextInt()
+        else {
+            random.nextInt()
+            loop(random, int - 1)
+        }
+    }
+}
